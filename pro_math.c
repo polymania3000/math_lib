de7dbd513816c9522c872b3d2cd3bda9f258bf11
Song Chuntao <songchuntao3000@163.com>
2018-01-27 15:00:10 +0800

    add abs, max function in math module

--- pro_math.c
+++ pro_math.c
@@ -1,3 +1,13 @@
 
 #include "pro_math.h"
 
+uint32_t Math_Abs (int32_t a_u32_value)
+{
+    return ( a_u32_value>=0 ? (uint32_t)a_u32_value : (uint32_t)(-a_u32_value) );
+}
+
+
+int32_t Math_Max (int32_t a_u32_value1, int32_t a_u32_value2)
+{
+    return ( a_u32_value1>=a_u32_value2 ? a_u32_value1 : a_u32_value2 );
+}
--- pro_math.h
+++ pro_math.h
@@ -2,5 +2,9 @@
 #ifndef _PRO_MATH_H
 #define _PRO_MATH_H
 
+#include <stdint.h>
+
+uint32_t Math_Abs (int32_t a_u32_value);
+int32_t Math_Max (int32_t a_u32_value1, int32_t a_u32_value2);
 
 #endif
